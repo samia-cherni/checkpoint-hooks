0f03fe3da29ceddc0715334299606d0ac210f81a
samia-cherni <samiacherni@outlook.com>
2021-05-26 17:08:54 +0100

    updating

--- src/index.js
+++ src/index.js
@@ -5,5 +5,7 @@ import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
-ReactDOM.render(<App />,document.getElementById('root'));
+ReactDOM.render(<React.StrictMode>
+                <App />
+                </React.StrictMode>,document.getElementById('root'));
 
